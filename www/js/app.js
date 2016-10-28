bab00d6658833a6ca2ea4237e4b07e749a8be537
ShawAcademyMobileApp <ShawMAD@Marks-iMac.local>
2016-10-28 13:07:46 +0100

    Initial Development
    Adding in Functionality for Task Lists

--- www/js/app.js
+++ www/js/app.js
@@ -1,38 +1,19 @@
-var taskList = [];
 
-function addTask()
-{
-    var task = document.getElementById("inputValue").value;
-    document.getElementById("inputValue").value ="";
-    taskList.push(task);
-    displayList();
-}
 
-function removeTask()
-{
-    var arrayId = this.getAttribute("id");
-    taskList.splice(arrayId, 1);
-    displayList();
-}
 
 function displayList()
 {
-    var taskListContent = "<ul>";
-    for(var i = 0; i < taskList.length; i++)
-    {
-        taskListContent += "<li>"+taskList[i]+"  <button class='deleteTask' id='"+i+"'>Remove</button></li>";
-
-    }
-    taskListContent += "</ul>";
-    document.getElementById("TaskListValue").innerHTML = taskListContent;
-
-    var removeBtnList = document.getElementsByClassName("deleteTask");
-    for(i = 0; i<removeBtnList.length; i++)
-    {
-        removeBtnList[i].addEventListener("click", removeTask);
-    }
-
+    //Display Dynamic HTML Lists
+    //UL & LI Tags
+    //Remove Button Displays
 }
+function addTask()
+{
+    //Add in the Tasks from the input field in our HTML File
+    //Display the latest list of tasks
+    displayList();
+}
+
 
 displayList();
 document.getElementById("Add").addEventListener("click", addTask);
